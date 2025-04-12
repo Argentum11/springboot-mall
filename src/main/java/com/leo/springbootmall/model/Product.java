54fc0c3e2659949d9719cf861454e75d580ca9b6
Argentum11 <92793837+Argentum11@users.noreply.github.com>
2025-04-12 21:30:30 +0800

    refactor(product): replace category string with ProductCategory enum in Product class

--- src/main/java/com/leo/springbootmall/model/Product.java
+++ src/main/java/com/leo/springbootmall/model/Product.java
@@ -1,11 +1,13 @@
 package com.leo.springbootmall.model;
 
+import com.leo.springbootmall.constant.ProductCategory;
+
 import java.util.Date;
 
 public class Product {
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
@@ -20,11 +22,11 @@ public class Product {
         this.productName = productName;
     }
 
-    public String getCategory() {
+    public ProductCategory getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(ProductCategory category) {
         this.category = category;
     }
 
